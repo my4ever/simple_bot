137468a2e65eae3e9e8b0f093045503aa4a49354
Ruslan Yakimov <goodbuhlnr@yandex.ua>
2021-06-16 16:32:39 +0300

    adding username into DB done

--- bot/bot.py
+++ bot/bot.py
@@ -1,15 +1,18 @@
 import requests
 from time import sleep
-import os
 
-TOKEN =
-
+TOKEN = None
+with open('/home/ruslan/Dev/simple_projects/simple_bot/.env', 'r') as token:
+    for i in token:
+        TOKEN = str(i)
 url = f'https://api.telegram.org/bot{TOKEN}/'
+
 
 def get_updates_json(request):
     """Getting update from server."""
     response = requests.get(request + 'getUpdates')
     return response.json()
+
 
 def last_update(data):
     """Getting last update info."""
@@ -17,21 +20,26 @@ def last_update(data):
     total_updates = len(results) - 1
     return results[total_updates]
 
+
 def get_chat_id(update):
     """Getting chat id."""
     chat_id = update['message']['chat']['id']
     return chat_id
 
+
 def get_message(update):
     """Getting message."""
+    print(update)
     message = update['message']['text']
     return message
 
-def send_mess(chat, text):
+
+def send_message(chat, text):
     """Sending the message."""
     params = {'chat_id': chat, 'text': text}
     response = requests.post(url + 'sendMessage', data=params)
     return response
+
 
 def checking_for_user(update):
     """Checking for username in database."""
@@ -42,20 +50,24 @@ def checking_for_user(update):
         if username not in names:
             add_user_to_db(username)
 
+
 def add_user_to_db(username):
     """Adding username into database."""
     with open('db.txt', 'a') as database:
         database.write('\n' + username)
 
+
 def main():
     update_id = last_update(get_updates_json(url))['update_id']
     while True:
         if update_id == last_update(get_updates_json(url))['update_id']:
-           send_mess(get_chat_id(last_update(get_updates_json(url))),
-                     get_message(last_update(get_updates_json(url))))
+           send_message(
+               get_chat_id(last_update(get_updates_json(url))),
+               get_message(last_update(get_updates_json(url))))
            checking_for_user(last_update(get_updates_json(url)))
            update_id += 1
     sleep(1)
+
 
 if __name__ == '__main__':
     main()
